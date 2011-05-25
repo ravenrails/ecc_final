a0b6562c833eac314c8ea38f4a689820cd28d402
Johann Tan <johanntan@ymail.com>
2011-05-25 17:06:18 +0800

    tag resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,13 @@
 Bard::Application.routes.draw do
 
-<<<<<<< HEAD
 
-
-
-  get "tags/index"
-
-  get "tags/show"
-
-  get "tags/edit"
-
-  get "tags/new"
-
-=======
->>>>>>> 522e5c8a874383f566ad5e75d938574bd15af100
-  resources :members 
-  resources :projects 
+  resources :tags
+  resources :members
+  resources :projects
 
   devise_for :users
 
-  root :to => 'projects#index'     
+  root :to => 'projects#index'
 
   # also Ensure you have flash messages in app/views/layouts/application.html.erb.
   #   For example:
