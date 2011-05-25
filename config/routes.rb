dc8d7d35f68e052595240fe2d36d7e1f90947c3d
Alvin Garcia <alvindgarcia@yahoo.com>
2011-05-25 13:50:12 +0800

    Setup user migrate

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Bard::Application.routes.draw do
 
   devise_for :users
 
-  # root :to =>        need default page for devise
+  root :to => 'project#index'     
 
   # also Ensure you have flash messages in app/views/layouts/application.html.erb.
   #   For example:
--- db/migrate/20110525051011_devise_create_users.rb
+++ db/migrate/20110525051011_devise_create_users.rb
@@ -5,6 +5,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.recoverable
       t.rememberable
       t.trackable
+      t.string :username
+      t.string :lastname
+      t.string :firstname
+      t.date :bday
 
       # t.encryptable
       # t.confirmable
@@ -15,7 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    add_index :users, :username,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
