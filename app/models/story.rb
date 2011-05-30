15489f136924d41d6921c4b56b16b8d9eb5e5db3
Johann Tan <johanntan@ymail.com>
2011-05-30 09:58:46 +0800

    merge before pull

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,16 +5,16 @@ class Story < ActiveRecord::Base
   has_many    :comments
   has_many    :ratings
   belongs_to  :creator, :class_name => "User"
-  
   has_one :user
-  
+  validates_length_of :name, :maximum => 140
+
   def tags_s
     str = ''
-    
+
     self.tags.each do |tag|
       str << (str.empty? ? '': ', ') << tag.name
     end
-    
+
     str
   end
 end
